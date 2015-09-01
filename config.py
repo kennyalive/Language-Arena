fe80721a1ca0bbbe68cd1ee9fcf360b62d1972a9
Artem Kharytoniuk <artemalive@gmail.com>
2015-09-01 20:49:13 +0300

    config.py prototyping

--- config.py
+++ config.py
@@ -1,2 +1,23 @@
-
-
+configs = [
+    {
+        'lang' : 'cpp',
+        'build_flavours' : [{
+            'name' : 'msvc2015',
+            'vc_vars' : r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat'
+        }]
+    },
+    {
+        'lang' : 'd',
+        'build_flavours' : [ {
+            'name' : 'gdc',
+            'compiler' : r'C:\\D\\gdc\\bin\\gdc.exe'
+        }]
+    },
+    {
+        'lang' : 'go',
+        'build_flavours' : [{
+            'name' : 'go',
+            'compiler' : r'C:\\Go\\bin\\go.exe'
+        }]
+    }
+]
