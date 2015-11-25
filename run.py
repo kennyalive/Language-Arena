3acecf5379b8725189e1af9cd5a10dd9edd00a7e
Artem Kharytoniuk <artemalive@gmail.com>
2015-11-26 00:37:08 +0200

    Launcher update: put score calculation in Scorecard class.

--- run.py
+++ run.py
@@ -5,11 +5,62 @@ import subprocess
 import sys
 import time
 
-from config import benchmark_builders
+from collections import defaultdict
+
+import config
 
 BUILD_DIR = 'build'
 BENCHMARKS_DIR = 'benchmarks'
 DATA_DIR = 'data'
+
+class Scorecard:
+    def __init__(self):
+        self.results = defaultdict(lambda: {'score': 0, 'time': 0.0})
+
+    def on_benchmark_start(self, benchmark):
+        self.benchmark_timings = {}
+
+        simple_benchmark = is_simple_benchmark(benchmark)
+        self.points = [10, 5] if simple_benchmark else [20, 10]
+
+    def register_benchmark_time(self, language, time):
+        self.benchmark_timings[language] = time
+
+    def on_benchmark_end(self):
+        if not self.benchmark_timings:
+            return
+
+        sorted_benchmark_timings = sorted(self.benchmark_timings.items(), key=lambda x: x[1])
+
+        time_scale = 1.0 / sorted_benchmark_timings[0][1]
+        comparison_lang_str = ''
+        comparison_time_str = ''
+
+        for i, (language, time) in enumerate(sorted_benchmark_timings):
+            earned_points = self.points[i] if i < len(self.points) else 0
+
+            result = self.results[language]
+            result['score'] += earned_points
+            result['time'] += time
+
+            if i < 2:
+                position = 'First' if i == 0 else 'Second'
+                print('{0} place: {1}. +{2} points'.format(position, language, earned_points))
+        
+            comparison_lang_str += language
+            comparison_time_str +=  '{:.2f}'.format(time * time_scale)
+            if i != len(sorted_benchmark_timings) - 1:
+                comparison_lang_str += ' : '
+                comparison_time_str += ' : '
+
+        print('Relative timing: {0} = {1}'.format(comparison_lang_str, comparison_time_str))
+
+    def print_summary(self):
+        sorted_results = sorted(self.results.items(), key=lambda x: x[1]['score'], reverse=True)
+        print('')
+        print('Summary:')
+        for i, (language, result) in enumerate(sorted_results):
+            print('Place ' + str(i + 1) + '. ' + language + ' with ' + str(result['score']) + ' points')
 
 def get_benchmark_languages(benchmark):
     directories = os.listdir(os.path.join(BENCHMARKS_DIR, benchmark))
@@ -19,25 +70,28 @@ def is_simple_benchmark(benchmark):
     tag_file = os.path.join(BENCHMARKS_DIR, benchmark, 'simple')
     return os.path.exists(tag_file)
 
-def build_benchmark_with_builder(benchmark, language, builder_config):
-    builder_func_name = builder_config['builder']
-    builders_module = importlib.import_module('scripts.' + language)
+def get_language_configuration(language):
+    return next((c for c in config.languages if c['language'] == language), None)
 
-    if builder_func_name not in dir(builders_module):
+def build_benchmark_with_configuration(benchmark, language, build_configuration):
+    language_module = importlib.import_module('scripts.' + language)
+    builder_func_name = build_configuration['builder']
+
+    if builder_func_name not in dir(language_module):
         print('failed to find builder function: ' + builder_func_name)
         sys.exit()
 
-    output_dir_abs = os.path.abspath(os.path.join(BUILD_DIR, benchmark,
-        language, builder_config['name']))
-    language_dir = os.path.join(BENCHMARKS_DIR, benchmark, language)
+    output_dir = os.path.join(BUILD_DIR, benchmark, language, build_configuration['name'])
+    output_dir_abs = os.path.abspath(output_dir)
 
+    language_dir = os.path.join(BENCHMARKS_DIR, benchmark, language)
     build_launcher_script = (
         "from scripts." + language +
         " import " + builder_func_name + "\n" +
         builder_func_name + "(r'" +
         language_dir + "', r'" +
         output_dir_abs + "'," +
-        "eval(r'''" + repr(builder_config) + "'''))"
+        "eval(r'''" + repr(build_configuration) + "'''))"
     )
 
     os.makedirs(output_dir_abs)
@@ -46,45 +100,34 @@ def build_benchmark_with_builder(benchmark, language, builder_config):
 def build_benchmark(benchmark):
     os.environ['PYTHONPATH'] = os.path.dirname(os.path.realpath(__file__))
     for language in get_benchmark_languages(benchmark):
-        language_builders = benchmark_builders.get(language)
-        if language_builders is None:
-            print('builders for ' + language +
-                ' are not specified in config.py')
+        language_configuration = get_language_configuration(language)
+        if language_configuration is None:
+            print('configuration for {0} is not specified in config.py'.format(language))
             continue
-        for builder in language_builders:
-            build_benchmark_with_builder(benchmark, language, builder)
+        for build_configuration in language_configuration['build_configurations']:
+            build_benchmark_with_configuration(benchmark, language, build_configuration)
 
-def run_benchmark(benchmark, scores_table):
+def run_benchmark(benchmark, scorecard):
+    print('------ Running ' + benchmark + ' ------')
+    scorecard.on_benchmark_start(benchmark)
     data_dir = os.path.join(BENCHMARKS_DIR, benchmark, DATA_DIR)
 
-    timing = []
     for language in get_benchmark_languages(benchmark):
+        language_configuration = get_language_configuration(language)
+        if language_configuration is None:
+            continue
+
         language_best_time = sys.float_info.max
 
-        for builder in benchmark_builders.get(language, []):
-            print(language + '/' + builder['name'])
-            executable = os.path.join(BUILD_DIR, benchmark, language,
-                builder['name'], 'benchmark.exe')
+        for build_configuration in language_configuration['build_configurations']:
+            print(language + '/' + build_configuration['name'])
+            executable = os.path.join(BUILD_DIR, benchmark, language, build_configuration['name'], 'benchmark.exe')
             benchmark_result = subprocess.call([executable, data_dir])
             elapsed_time = benchmark_result / 1000.0
-            language_best_time = min(language_best_time, elapsed_time)
             print("{:.3f}".format(elapsed_time))
-
-        timing.append((language, language_best_time))
-
-    simple = is_simple_benchmark(benchmark)
-    first_place_score = 10 if simple else 20
-    second_place_score = 5 if simple else 10
-
-    timing.sort(key = lambda x: x[1])
-    if len(timing) > 0:
-        first_language = timing[0][0]
-        scores_table[first_language] = scores_table.get(first_language, 0) + first_place_score
-        print('First place: {0}. +{1} points'.format(first_language, first_place_score))
-    if len(timing) > 1:
-        second_language = timing[1][0]
-        scores_table[second_language] = scores_table.get(second_language, 0) + second_place_score
-        print('Second place: {0}. +{1} points'.format(second_language, second_place_score))
+            language_best_time = min(language_best_time, elapsed_time)
+        scorecard.register_benchmark_time(language, language_best_time)
+    scorecard.on_benchmark_end()
 
 if __name__ == '__main__':
     if os.path.exists(BUILD_DIR):
@@ -103,14 +146,11 @@ if __name__ == '__main__':
     for benchmark in benchmarks:
         build_benchmark(benchmark)
 
-    scores_table = {}
-
+    scorecard = Scorecard()
     print("")
-    for benchmark in benchmarks:
-        print('------ Running ' + benchmark + ' ------')
-        run_benchmark(benchmark, scores_table)
 
-    sorted_scores_table = sorted(scores_table.items(), key=lambda x: x[1], reverse=True)
-    print('')
-    for i, score_info in enumerate(sorted_scores_table):
-        print('Place ' + str(i + 1) + '. ' + score_info[0] + ' with ' + str(score_info[1]) + ' points')
+    for benchmark in benchmarks:
+        run_benchmark(benchmark, scorecard)
+
+    scorecard.print_summary()
+
