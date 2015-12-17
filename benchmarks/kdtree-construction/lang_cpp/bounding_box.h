368bd508d5375eab78a8460f29f68546aa9e24fe
Artem Kharytoniuk <artemalive@gmail.com>
2015-12-18 00:12:23 +0200

    More reformatting.

--- benchmarks/kdtree-construction/lang_cpp/bounding_box.h
+++ benchmarks/kdtree-construction/lang_cpp/bounding_box.h
@@ -6,29 +6,22 @@
 #include <limits>
 
 template <typename T>
-struct TBoundingBox
-{
+struct TBoundingBox {
     TVector<T> minPoint;
     TVector<T> maxPoint;
 
     TBoundingBox()
-        : minPoint(TVector<T>(std::numeric_limits<T>::infinity()))
-        , maxPoint(TVector<T>(-std::numeric_limits<T>::infinity()))
+        : minPoint(TVector<T>(std::numeric_limits<T>::infinity())),
+          maxPoint(TVector<T>(-std::numeric_limits<T>::infinity()))
     {
     }
 
     TBoundingBox(TVector<T> minPoint, TVector<T> maxPoint)
-        : minPoint(minPoint)
-        , maxPoint(maxPoint)
+        : minPoint(minPoint), maxPoint(maxPoint)
     {
     }
 
-    TBoundingBox(TVector<T> point)
-        : maxPoint(point)
-        , minPoint(point)
-    {
-    }
-
+    TBoundingBox(TVector<T> point) : maxPoint(point), minPoint(point) {}
     void extend(TVector<T> point)
     {
         minPoint.x = std::min(minPoint.x, point.x);
@@ -40,25 +33,18 @@ struct TBoundingBox
         maxPoint.z = std::max(maxPoint.z, point.z);
     }
 
-    static TBoundingBox<T> boundsUnion(const TBoundingBox<T>& bounds, const TBoundingBox<T>& bounds2)
+    static TBoundingBox<T> boundsUnion(const TBoundingBox<T>& bounds,
+                                       const TBoundingBox<T>& bounds2)
     {
-        return TBoundingBox<T>
-        (
-            TVector<T>
-            (
-                std::min(bounds.minPoint.x, bounds2.minPoint.x),
-                std::min(bounds.minPoint.y, bounds2.minPoint.y),
-                std::min(bounds.minPoint.z, bounds2.minPoint.z)
-            ),
-            TVector<T>
-            (
-                std::max(bounds.maxPoint.x, bounds2.maxPoint.x),
-                std::max(bounds.maxPoint.y, bounds2.maxPoint.y),
-                std::max(bounds.maxPoint.z, bounds2.maxPoint.z)
-            )
-        );
+        return TBoundingBox<T>(
+            TVector<T>(std::min(bounds.minPoint.x, bounds2.minPoint.x),
+                       std::min(bounds.minPoint.y, bounds2.minPoint.y),
+                       std::min(bounds.minPoint.z, bounds2.minPoint.z)),
+            TVector<T>(std::max(bounds.maxPoint.x, bounds2.maxPoint.x),
+                       std::max(bounds.maxPoint.y, bounds2.maxPoint.y),
+                       std::max(bounds.maxPoint.z, bounds2.maxPoint.z)));
     }
 };
 
-using BoundingBox   = TBoundingBox<double>;
+using BoundingBox = TBoundingBox<double>;
 using BoundingBox_f = TBoundingBox<float>;
