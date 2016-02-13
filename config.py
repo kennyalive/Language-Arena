71fa2b86cd4c73dc733e7726d239714407211d3f
Artem Kharytoniuk <artemalive@gmail.com>
2016-02-13 10:59:19 +0200

    Updated compiler configurations script to be more user-friendly.

--- config.py
+++ config.py
@@ -1,34 +1,31 @@
-# Please update these paths according to your environment.
-#
-# It is not required to configure all compilers here, 
-# any subset will work (even empty).
-# It is okay to leave configurations for missing compilers as it is,
-# they will be skipped by the benchamrk.
-#
-# On Linux the following compilers are supported:
-# C++: gcc, clang
-# D: dmd, gdc, ldc
-# Go: go, gccgo
-#
-# On Windows the following compilers are supported:
-# C++: msvc,
-# D: dmd, gdc, ldc
-# Go : go
+import os
 
-compilers = {
-    # example of compilers configuration for Linux
-    'gcc'   : r'/usr/bin/g++',
-    'clang' : r'/usr/bin/clang++-3.6',
-    'dmd'   : r'/usr/bin/rdmd',
-    'gdc'   : r'/usr/local/gdc/bin/gdc',
-    'ldc'   : r'/usr/local/ldc/bin/ldc2',
-    'go'    : r'/usr/local/go/bin/go',
-    'gccgo' : r'/usr/bin/gccgo',
+# USAGE NOTES:
+#
+# Please update compiler paths according to your environment.
+#
+# It is NOT required to configure all supported compilers - any subset will work (even empty).
+# It is OKAY to have entries with invalid paths for some compilers, they will be skipped.
 
-    # example of compilers configuration for Windows
-    #'msvc'  : r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat',
-    #'dmd'   : r'C:\D\dmd2\windows\bin\rdmd.exe',
-    #'gdc'   : r'C:\D\gdc\bin\gdc.exe',
-    #'ldc'   : r'C:\D\ldc\bin\ldc2.exe',
-    #'go'    : r'C:\Go\bin\go.exe',
-}
+compilers = {}
+
+if os.name == 'nt':
+    # Windows configuration example
+    compilers = {
+        'msvc'  : r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat',
+        'dmd'   : r'C:\D\dmd2\windows\bin\rdmd.exe',
+        'gdc'   : r'C:\D\gdc\bin\gdc.exe',
+        'ldc'   : r'C:\D\ldc\bin\ldc2.exe',
+        'go'    : r'C:\Go\bin\go.exe',
+    }
+else:
+    # Linux/OS X configuration example
+    compilers = {
+        'gcc'   : r'/usr/bin/g++',
+        'clang' : r'/usr/bin/clang++-3.6',
+        'dmd'   : r'/usr/bin/rdmd',
+        'gdc'   : r'/usr/local/gdc/bin/gdc',
+        'ldc'   : r'/usr/local/ldc/bin/ldc2',
+        'go'    : r'/usr/local/go/bin/go',
+        'gccgo' : r'/usr/bin/gccgo',
+    }
