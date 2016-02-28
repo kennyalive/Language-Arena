4e9eb81fa97114e8408d89025bc5dc9717c83a1f
Artem Kharytoniuk <artemalive@gmail.com>
2016-02-28 22:58:34 +0200

    Use ordered dictionary to store compilers configuration. Ordered dictionary allows to print the list of compilers always in the same order (also matches the order specified by configuration file).

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from collections import OrderedDict
 
 # USAGE NOTES:
 #
@@ -7,25 +8,25 @@ import os
 # It is not required to configure all supported compilers - any subset will work (even empty).
 # It is okay to have entries with invalid paths for some compilers, they will be skipped.
 
-compilers = {}
+compilers = None
 
 if os.name == 'nt':
     # Windows configuration example
-    compilers = {
-        'msvc'  : r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat',
-        'dmd'   : r'C:\D\dmd2\windows\bin\rdmd.exe',
-        'gdc'   : r'C:\D\gdc\bin\gdc.exe',
-        'ldc'   : r'C:\D\ldc\bin\ldc2.exe',
-        'go'    : r'C:\Go\bin\go.exe',
-    }
+    compilers = OrderedDict([
+        ('msvc', r'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat'),
+        ('dmd' , r'C:\D\dmd2\windows\bin\rdmd.exe'),
+        ('gdc' , r'C:\D\gdc\bin\gdc.exe'),
+        ('ldc' , r'C:\D\ldc2\bin\ldc2.exe'),
+        ('go'  , r'C:\Go\bin\go.exe'),
+    ])
 else:
     # Linux/OS X configuration example
-    compilers = {
-        'gcc'   : r'/usr/bin/g++',
-        'clang' : r'/usr/bin/clang++-3.6',
-        'dmd'   : r'/usr/bin/rdmd',
-        'gdc'   : r'/usr/local/gdc/bin/gdc',
-        'ldc'   : r'/usr/local/ldc/bin/ldc2',
-        'go'    : r'/usr/local/go/bin/go',
-        'gccgo' : r'/usr/bin/gccgo',
-    }
+    compilers = OrderedDict([
+        ('gcc'  , r'/usr/bin/g++'),
+        ('clang', r'/usr/bin/clang++-3.6'),
+        ('dmd'  , r'/usr/bin/rdmd'),
+        ('gdc'  , r'/usr/local/gdc/bin/gdc'),
+        ('ldc'  , r'/usr/local/ldc/bin/ldc2'),
+        ('go'   , r'/usr/local/go/bin/go'),
+        ('gccgo', r'/usr/bin/gccgo'),
+    ])
